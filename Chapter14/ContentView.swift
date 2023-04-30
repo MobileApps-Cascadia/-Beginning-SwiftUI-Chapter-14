90977c518297fa143c62f3bee4ab2eb4ddb6181d
Mike Panitz <michpa@hotmail.com>
2023-04-30 09:34:13 -0700

    push template

--- Chapter14/ContentView.swift
+++ Chapter14/ContentView.swift
@@ -2,18 +2,26 @@
 //  ContentView.swift
 //  Chapter14
 //
-//  Created by Mike Panitz on 4/30/23.
+//  Created by Mike Panitz on 4/18/23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        TabView {
+            Text("Item 1")
+                .tabItem {
+                    Image(systemName: "1.circle")
+                    Text(" ")
+                }
+            Text("Item 2")
+                .tabItem {
+                    Image(systemName: "2.circle")
+                    Text(" ")
+                }
+            
         }
         .padding()
     }
