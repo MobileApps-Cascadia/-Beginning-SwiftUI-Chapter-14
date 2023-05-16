4ee7b952b3cc7fde4cba369012896fd5e6731dc4
Tori Lew <torirlew@gmail.com>
2023-05-16 16:20:00 -0700

    1-3 in exercise

--- Chapter14/Exercises.swift
+++ Chapter14/Exercises.swift
@@ -2,14 +2,31 @@ import SwiftUI
 
 
 struct ExercisesView: View {
-    @State var messageOne = ""
-    
+    @State var username = ""
+    @State var name = ""
+    @State var modeOn = true
+    @State var mode = "Easy"
     var body: some View {
         VStack {
             Text("Exercises")
                 .font(.largeTitle)
-            Text("Your form goes here")
-            Text("More text after the form")
+            VStack {
+                Form {
+                    Text("Welcome to a fun rp game")
+                    TextField("Username", text: $username)
+                    TextField("Actual name", text: $name)
+                    Toggle(modeOn ? "Mode: Easy" : "Mode: Hard", isOn: $modeOn)
+                }
+            }
+            if self.modeOn{
+                Text("Welcome \(name), your username is \(username) and you have chosen Easy Mode")
+                    .padding()
+            } else{
+                Text("Welcome \(name), your username is \(username) and you have chosen Hard Mode")
+                    .padding()
+            }
+            
+            
         }
     }
 }
